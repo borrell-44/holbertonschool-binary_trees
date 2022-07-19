e3a1b54efc3a945cfe0f9189cbe37a13beb09992
borrell-44 <alexanderborrell44@hotmail.com>
2022-07-19 19:05:25 -0400

    Committing task 5

---
+++ 5-binary_tree_is_root.c
@@ -0,0 +1,20 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_is_root - checks if node is root
+ *
+ * @node: node to check
+ * 
+ * Return: 1 if node is root and 0 for leaf
+ */
+
+int binary_tree_is_root(const binary_tree_t *node)
+{
+    if (!node)
+        return (0);
+    
+    if (node->left != NULL && node->right != NULL)
+        return (1);
+    else
+        return (0);
+}
