6005393b379ee16440bf8b845badfb9ae5100af9
borrell-44 <alexanderborrell44@hotmail.com>
2022-07-20 12:00:39 -0400

    Committing task 11

---
+++ 11-binary_tree_size.c
@@ -0,0 +1,21 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_size - measures the size of a binary tree
+ *
+ * @tree: node or leaf of a tree
+ *
+ * Return: size of binary tree
+ */
+
+size_t binary_tree_size(const binary_tree_t *tree)
+{
+	int count;
+	if (!tree)
+		return (0);
+	
+	count = binary_tree_size(tree->left);
+	count += binary_tree_size(tree->right) + 1;
+
+	return (count);
+}
