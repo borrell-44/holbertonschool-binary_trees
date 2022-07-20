ec4eaca55db660ae09913fcfee1ae85969505b75
borrell-44 <alexanderborrell44@hotmail.com>
2022-07-20 10:05:19 -0400

    Hot Fix

--- 0-binary_tree_node.c
+++ 0-binary_tree_node.c
@@ -8,17 +8,17 @@
  * Return: binary_tree_t node
  */
 
- binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
- {
-    binary_tree_t *new;
+binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
+{
+	binary_tree_t *new;
 
-    new = malloc(sizeof(binary_tree_t));
-    if (new == NULL)
-        return (NULL);
-    new->n = value;
-    
-    new->parent = parent;
-    new->left = NULL;
-    new->right = NULL;
-    return (new);
- }
+	new = malloc(sizeof(binary_tree_t));
+	if (new == NULL)
+		return (NULL);
+	new->n = value;
+
+	new->parent = parent;
+	new->left = NULL;
+	new->right = NULL;
+	return (new);
+}
