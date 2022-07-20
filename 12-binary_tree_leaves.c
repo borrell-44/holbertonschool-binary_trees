c95a829266c555a989bb2e65fbd56a805547d6b7
borrell-44 <alexanderborrell44@hotmail.com>
2022-07-20 15:19:52 -0400

    Committing task 12

---
+++ 12-binary_tree_leaves.c
@@ -0,0 +1,25 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_leaves - counts the leaves in a binary tree
+ *
+ * @tree: node or leaf of a tree
+ *
+ * Return: leave count
+ */
+
+size_t binary_tree_leaves(const binary_tree_t *tree)
+{
+	int num = 0;
+
+	if (!tree)
+		return (0);
+	
+	if (tree->left == NULL || tree->right == NULL)
+		return (1);
+
+	num += binary_tree_leaves(tree->left);
+	num += binary_tree_leaves(tree->right);
+
+	return (num);
+}
