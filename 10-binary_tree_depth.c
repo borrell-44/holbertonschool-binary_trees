317c0e899b41cb91d19bf88af7c9086fb1efbab6
borrell-44 <alexanderborrell44@hotmail.com>
2022-07-20 11:49:26 -0400

    Committing task 10

---
+++ 10-binary_tree_depth.c
@@ -0,0 +1,20 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_depth - measures the depth of a binary tree
+ *
+ * @tree: node or leaf of a tree
+ *
+ * Return: depth of tree
+ */
+
+size_t binary_tree_depth(const binary_tree_t *tree)
+{
+	if (!tree)
+		return (0);
+
+	if (tree->parent == NULL)
+		return (0);
+
+	return (binary_tree_depth(tree->parent) + 1);
+}
