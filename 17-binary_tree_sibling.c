956d54260baf4b379baf9c916d3308968d949467
borrell-44 <alexanderborrell44@hotmail.com>
2022-07-21 13:04:02 -0400

    Committing task 17

---
+++ 17-binary_tree_sibling.c
@@ -0,0 +1,51 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_siblings - finds the sibling of a node
+ *
+ * @node: place to find sibling
+ *
+ * Return: sibling node
+ */
+
+binary_tree_t *binary_tree_sibling(binary_tree_t *node)
+{
+	int num = 0, i;
+	binary_tree_t *tmp, *l, *r;
+
+	if (!node)
+		return (NULL);
+
+	if (node->parent == NULL)
+		return (NULL);
+
+	tmp = node;
+	while (tmp->parent != NULL)
+	{
+		num += 1;
+		tmp = tmp->parent;
+		if (tmp->left != NULL && tmp->right != NULL)
+			break;
+	}
+
+	l = tmp;
+	r = tmp;
+	for (i = 1; i <= num; i++)
+	{
+		if (l->left != NULL && r->right != NULL)
+		{
+			l = l->left;
+			r = r->right;
+
+			if (i == num)
+			{
+				if (l->n != node->n)
+					return (l);
+				if (r->n != node->n)
+					return (r);
+			}
+		}
+	}
+
+	return (NULL);
+}
