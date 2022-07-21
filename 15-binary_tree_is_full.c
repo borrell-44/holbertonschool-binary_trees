f02d98dca5be3a99413c33ea3581be8c76cb1cb0
borrell-44 <alexanderborrell44@hotmail.com>
2022-07-21 09:01:26 -0400

    Fixing syntax

--- 15-binary_tree_is_full.c
+++ 15-binary_tree_is_full.c
@@ -12,7 +12,7 @@ int binary_tree_is_full(const binary_tree_t *tree)
 {
 	if (!tree)
 		return (0);
-	
+
 	if (tree->left != NULL && tree->right == NULL)
 		return (0);
 	if (tree->left == NULL && tree->right != NULL)
