1cbfec529a129a13d3151972899317ba523534a4
borrell-44 <alexanderborrell44@hotmail.com>
2022-07-22 10:14:37 -0400

    Adding the betty format

--- 100-binary_trees_ancestor.c
+++ 100-binary_trees_ancestor.c
@@ -29,7 +29,8 @@ size_t binary_tree_depth(const binary_tree_t *tree)
  * Return: ancestor node
  */
 
-binary_tree_t *binary_trees_ancestor(const binary_tree_t *first, const binary_tree_t *second)
+binary_tree_t *binary_trees_ancestor(const binary_tree_t *first, 
+const binary_tree_t *second)
 {
 	int f, s;
 
