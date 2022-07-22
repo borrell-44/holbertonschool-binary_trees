8a069e46d994bbfb53e75d4261f48e3fd2d6231c
borrell-44 <alexanderborrell44@hotmail.com>
2022-07-22 07:48:11 -0400

    Simplifyng the code

--- 17-binary_tree_sibling.c
+++ 17-binary_tree_sibling.c
@@ -10,42 +10,13 @@
 
 binary_tree_t *binary_tree_sibling(binary_tree_t *node)
 {
-	int num = 0, i;
-	binary_tree_t *tmp, *l, *r;
-
-	if (!node)
+	if (node == NULL)
 		return (NULL);
-
+	
 	if (node->parent == NULL)
 		return (NULL);
-
-	tmp = node;
-	while (tmp->parent != NULL)
-	{
-		num += 1;
-		tmp = tmp->parent;
-		if (tmp->left != NULL && tmp->right != NULL)
-			break;
-	}
-
-	l = tmp;
-	r = tmp;
-	for (i = 1; i <= num; i++)
-	{
-		if (l->left != NULL && r->right != NULL)
-		{
-			l = l->left;
-			r = r->right;
-
-			if (i == num)
-			{
-				if (l->n != node->n)
-					return (l);
-				if (r->n != node->n)
-					return (r);
-			}
-		}
-	}
-
-	return (NULL);
+	
+	if (node->parent->left == node)
+		return (node->parent->right);
+	return (node->parent->left);
 }
